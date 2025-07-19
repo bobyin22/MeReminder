272b70972ee68dda24246502d2b207884fa5b72f
bobyin <bobyin22@gmail.com>
2025-07-19 22:14:23 +0800

    Initial Commit

---
+++ MeReminder/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MeReminder
+//
+//  Created by 邱慧珊 on 7/19/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
